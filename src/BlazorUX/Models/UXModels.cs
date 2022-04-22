e9a0d9114f34e2f8c607ac5bf3f30b1282324a97
JohnPKosh <devprohome@live.com>
2022-04-22 06:25:38 -0400

    add BlazorUX

---
+++ src/BlazorUX/Models/UXModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorUX.Models
+{
+  public enum HeaderSize
+  {
+    h1 = 1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6
+  }
+
+  public class HeaderTagModel
+  {
+    public string Text { get; set; }
+
+    public HeaderSize Size { get; set; }
+  }
+}
